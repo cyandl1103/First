a8226ad582d37c90e8d143827d34084d10cf1460
admin <admin>
2022-11-07 17:01:08 +0900

    first

---
+++ query.sql
@@ -0,0 +1,46 @@
+
+
+-- create table --
+CREATE TABLE `login` (
+  `id` varchar(20) NOT NULL,
+  `pw` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `post` (
+  `num` int NOT NULL,
+  `textTitle` varchar(45) DEFAULT NULL,
+  `textWriting` varchar(500) DEFAULT NULL,
+  `textDate` datetime DEFAULT NULL,
+  `author` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`num`),
+  KEY `author` (`author`),
+  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`author`) REFERENCES `login` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+create table post (
+	id int,
+	textTitle varchar(45),
+    textWriting varchar(500),
+    textDate datetime,
+    author varchar(20),
+    primary key (id),
+    foreign key (author) references login(id)
+);
+
+-- alter table --
+alter table post change column id num int;
+
+-- insert values --
+	-- 유저 추가 --
+insert into login (id, pw, name) 
+	values('admin', 'admin', 'admin');
+	
+    -- 게시물 추가 -- 
+insert into post (num, textTitle, textWriting, textDate, author) 
+	values(1, 'example title', 'example writing', curdate(), 'admin');
+    
+-- select --
+select * from login;
+select  * from post;
